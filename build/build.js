0034dd08764ba17786bd52ad1e6d43c01c05b3c9
yuchangchang <yuchang251314@163.com>
2023-02-18 22:07:31 +0800

    other:build

--- build/build.js
+++ build/build.js
@@ -1,7 +1,7 @@
 const { execSync } = require("child_process");
 const dockerImageName = "yuxiaosen/mshop-web";
 
-execSync(`npm run build && docker buildx build \
+execSync(`docker buildx build \
 --platform linux/amd64,linux/arm64 \
 --push -t ${dockerImageName} .
 `);
